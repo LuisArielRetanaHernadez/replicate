f1ece74075cef4b2209de0c8441952d818659f0d
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-05 20:32:10 -0600

    creando la server action 'createPedriciton' donde se la pasamos en el action del form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,14 @@ import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 export default function Home() {
+  async function createPrediction(formData: FormData) {
+    'use server'
+
+    console.log(formData)
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form>
+      <form action={createPrediction}>
         <Input type="file" name="image" accept="image/*"/>
         <Textarea name="prompt"/>
         <Button>Crear</Button>
