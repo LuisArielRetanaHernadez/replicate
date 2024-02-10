f87132998e2dfa028d6b70d46e63c6872f6a7e19
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-09 20:55:37 -0600

    creando el componente FormContent dentro del page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,17 @@ import Image from "next/image";
 import { unstable_noStore as notStore } from "next/cache";
 import { useFormState } from "react-dom";
 import { createPrediction } from "@/actions";
-
+const FormContent = () => {
+  return (
+    <>
+      <Input type="text" name="image"
+        defaultValue='https://mueblescook.com.mx/wp-content/uploads/2020/10/Goal-Casaa2.gif'
+        className="border border-black"/>
+      <Textarea name="prompt" className="border border-black outline-none"/>
+      <Button type="submit">Crear</Button>
+    </>
+  )
+}
 export default function Home() {
   const [state, formAction] = useFormState(createPrediction, null)
   return (
@@ -16,11 +26,7 @@ export default function Home() {
         state?.output && <img src={state.output[1]} alt="output"/>
       }
       <form action={formAction} className="grid gap-4">
-        <Input type="text" name="image"
-          defaultValue='https://mueblescook.com.mx/wp-content/uploads/2020/10/Goal-Casaa2.gif'
-         className="border border-black"/>
-        <Textarea name="prompt" className="border border-black outline-none"/>
-        <Button type="submit">Crear</Button>
+        {FormContent()}
       </form>
     </main>
   );
