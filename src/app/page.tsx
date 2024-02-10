0b5a61338be8f15849eaa505e4ea438f7bf22d66
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-02-09 20:11:08 -0600

    agregando el estado useFormState

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import { Textarea } from "@/components/ui/textarea";
 import Image from "next/image";
 
 import { unstable_noStore as notStore } from "next/cache";
+import { useFormState } from "react-dom";
+import { createPrediction } from "@/actions";
 
 export default function Home() {
+  const [state, formAction] = useFormState(createPrediction, null)
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form className="grid gap-4">
+      <form action={formAction} className="grid gap-4">
         <Input type="text" name="image"
           defaultValue='https://mueblescook.com.mx/wp-content/uploads/2020/10/Goal-Casaa2.gif'
          className="border border-black"/>
